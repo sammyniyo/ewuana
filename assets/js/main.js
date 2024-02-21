100a901643c219880417f873ccd39bb9684f7d71
sammyniyo <samshakul@gmail.com>
2024-02-21 15:38:46 +0200

    new styles on header and products

--- assets/js/main.js
+++ assets/js/main.js
@@ -22,6 +22,12 @@ const navLink = document.querySelectorAll('.nav__link')
  navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
+const blurHeader = () =>{
+    const header = document.getElementById('header')
+    this.scrollY >= 50 ? header.classList.add('blur-header')
+                       : header.classList.remove('blur-header')
+}
+window.addEventListener('scroll', blurHeader)
 
 
 /*=============== SHOW SCROLL UP ===============*/ 

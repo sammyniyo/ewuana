92727d5f292f62e22555f266fab00e84252227ca
sammyniyo <samshakul@gmail.com>
2024-02-21 14:21:29 +0200

    New design

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,8 +1,25 @@
 /*=============== SHOW MENU ===============*/
+const navMenu = document.getElementById('nav-menu');
+const navToggle = document.getElementById('nav-toggle');
+const navClose = document.getElementById('nav-close');
 
+if (navToggle && navMenu && navClose) {
+    navToggle.addEventListener('click', () => {
+        navMenu.classList.add('show-menu');
+    });
+
+    navClose.addEventListener('click', () => {
+        navMenu.classList.remove('show-menu');
+    });
+}
 
 /*=============== REMOVE MENU MOBILE ===============*/
-
+const navLink = document.querySelectorAll('.nav__link')
+ const linkAction = () =>{
+    const navMenu = document.getElementById('nav-menu')
+    navMenu.classList.remove('show-menu')
+ }
+ navLink.forEach(n => n.addEventListener('click', linkAction))
 
 /*=============== ADD BLUR HEADER ===============*/
 

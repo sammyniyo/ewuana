612be3113f1673c19b05eed183bedd83be3cd3fc
sammyniyo <samshakul@gmail.com>
2024-02-22 15:36:25 +0200

    scroll and footer made

--- assets/js/main.js
+++ assets/js/main.js
@@ -30,10 +30,32 @@ const blurHeader = () =>{
 window.addEventListener('scroll', blurHeader)
 
 
-/*=============== SHOW SCROLL UP ===============*/ 
+/*=============== SHOW SCROLL UP ===============*/
+const scrollUp = () =>{
+    const scrollUp = document.getElementById('scroll-up')
+    this.scrollY >= 350 ? scrollUp.classList.add('show-scroll')
+                       : scrollUp.classList.add('show-scroll')
 
+} 
+window.addEventListener('scroll',scrollUp)
 
 /*=============== SCROLL SECTIONS ACTIVE LINK ===============*/
+const sections = document.querySelectorAll('section[id]')
+const scrollActive = () =>{
+    const scrollDown = window.scrollY
+    sections.forEach(current =>{
+        const sectionHeight = current.offsetHeight,
+        sectionTop = current.offsetTop -58,
+        sectionId = current.getAttribute('id'),
+        sectionClass = document.querySelector('.nav__menu a[href*=' + sectionId +']')
+    if(scrollDown > sectionTop && scrollDown <= sectionTop + sectionHeight){
+        sectionClass.classList.add('active-link')
+    }else{
+        sectionClass.classList.remove('active-link')
+    }
+    })
+}
+window.addEventListener('scroll', scrollActive)
 
 
 /*=============== SCROLL REVEAL ANIMATION ===============*/
